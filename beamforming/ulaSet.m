255a0231c524d891ad3a4faf3fd7404f26f83637
pianist112 <stevenh_sh@hotmail.com>
2015-02-01 13:05:39 -0800

    Update ulaSet.m

--- beamforming/ulaSet.m
+++ beamforming/ulaSet.m
@@ -1,14 +1,10 @@
 % ULA setting:
 % input parameters:
-%   anglePhi: look-angle of the array. Can also be matched file angle
-%   spacing: array spacing, in real distance (not lambda)
-%   carrierFreq: modulating frequency
-%   waveSpeed: most cases is c=3e8 but may vary in different medias
+%   anglePhi: look-angle of the array. anglePhi=arcsin(lambda*u/d), -.5<u<.5 so that -90<anglePhi<90
 %   arraySize: number of sensors in the ULA
 % output parameter:
 %   steerVector: steering vector at angle anglePhi 
 
-function steerVector = ulaSet(anglePhi, spacing, carrierFreq, waveSpeed, arraySize) 
-  lambda = waveSpeed/carrierFreq;
-  u = (spacing/lambda)*sin(anglePhi*pi/180);
-  steerVector = exp(-j*2*pi*u*(0:arraySize-1)/arraySize)/sqrt(M);
+function steerVector = ulaSet(anglePhi,arraySize) 
+   u = sin(anglePhi*pi/180);
+   steerVector = exp(-j*pi*[0:arraySize-1]'*u)/sqrt(arraySize);
